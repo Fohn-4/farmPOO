baf0cbf6df137c0249ae90d07fc6a724723bad73
Fohn-4 <renard_staifane@hotmail.fr>
2024-08-30 10:55:03 +0200

    Add: base python class files
    add animal, farm, farming, globals, main, market, player. Only animal have logic atm

---
+++ code/animals/animal.py
@@ -0,0 +1,31 @@
+import random
+from ..globals import G_turn
+
+class Animal:
+    def __init__(self):
+        self.weight = None
+        self.age = 0
+        self.gender = self.set_gender()
+        self.spawnTurn = G_turn
+
+    def set_gender(self):
+        #set male or female
+        rand = random.randint(0, 1)
+        gender = ""
+        if rand == 0:
+            gender = "male"
+        else:
+            gender = "female"
+        return gender
+
+    def set_age(self):
+        # Every 12 turn the animal aged one year
+        turnLived = G_turn - self.age
+        yearLived = 0
+        i = 0
+        while i < turnLived:
+            if turnLived % 12 == 0:
+                yearLived += 1
+        return yearLived
+
+
